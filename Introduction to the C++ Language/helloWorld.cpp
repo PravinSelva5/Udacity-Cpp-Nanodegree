33692072027ef15c33de57c0d7912c182dc34aaf
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2023-01-11 22:43:47 -0500

    first cpp program

---
+++ Introduction to the C++ Language/helloWorld.cpp
@@ -0,0 +1,9 @@
+// include tells the compiler to run these commands before executing the program
+// It is referred to as a preprocessor command
+#include <iostream>
+using std::cout;
+
+# main is the first function that is called by any Cpp program
+int main() {
+    cout << "Hello!" << "\n";
+}
