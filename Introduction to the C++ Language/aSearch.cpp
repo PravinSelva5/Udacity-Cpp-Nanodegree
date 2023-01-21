fdaa6975a59cae17dbeef95a039ff373d6784cfb
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2023-01-21 18:20:47 -0500

    added comparison and  adding nodes to open vector helper functions for A* search algorithm

--- Introduction to the C++ Language/aSearch.cpp
+++ Introduction to the C++ Language/aSearch.cpp
@@ -10,7 +10,7 @@ using std::string;
 using std::vector;
 using std::abs;
 
-enum class State {kEmpty, kObstacle};
+enum class State {kEmpty, kObstacle, kClosed};
 
 
 vector<State> ParseLine(string line) {
@@ -42,10 +42,48 @@ vector<vector<State>> ReadBoardFile(string path) {
   return board;
 }
 
-// TODO: Write the Search function stub here.
-vector<vector<State>> Search (vector<vector<State>> grid, int init[2], int goal[2]){
-	cout << "No path found" << "\n";
-	return std::vector<vector<State>> {};
+// TODO: Write function to compare the f-value of two nodes here
+bool Compare(vector<int> node1, vector<int> node2){
+	int f_val1 = node1[2] + node1[3];
+  	int f_val2 = node2[2] + node2[3];
+  
+  if (f_val1 > f_val2) {
+    return true;
+} else {
+    return false;
+}}
+// Calculate the manhattan distance
+int Heuristic(int x1, int y1, int x2, int y2) {
+  return abs(x2 - x1) + abs(y2 - y1);
+}
+
+
+/** 
+ * Add a node to the open list and mark it as open. 
+ */
+void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openlist, vector<vector<State>> &grid) {
+  // Add node to open vector, and mark grid cell as closed.
+  openlist.push_back(vector<int>{x, y, g, h});
+  grid[x][y] = State::kClosed;
+}
+
+
+/** 
+ * Implementation of A* search algorithm
+ */
+vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2]) {
+  // Create the vector of open nodes.
+  vector<vector<int>> open {};
+  
+  // Initialize the starting node.
+  int x = init[0];
+  int y = init[1];
+  int g = 0;
+  int h = Heuristic(x, y, goal[0],goal[1]);
+  AddToOpen(x, y, g, h, open, grid);
+
+  cout << "No path found!" << "\n";
+  return std::vector<vector<State>>{};
 }
 
 string CellString(State cell) {
@@ -65,6 +103,7 @@ void PrintBoard(const vector<vector<State>> board) {
   }
 }
 
+#include "test.cpp"
 
 int main() {
   int init[2]{0, 0};
@@ -72,4 +111,8 @@ int main() {
   auto board = ReadBoardFile("1.board");
   auto solution = Search(board, init, goal);
   PrintBoard(solution);
+  // Tests
+  TestHeuristic();
+  TestAddToOpen();
+  TestCompare();
 }
