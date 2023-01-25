59823e17060c3a9f93c363e7499218b85e1c7778
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2023-01-24 23:44:08 -0500

    updated A* search algorithm

--- Introduction to the C++ Language/aSearch.cpp
+++ Introduction to the C++ Language/aSearch.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>  // for sort
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -6,11 +7,15 @@
 using std::cout;
 using std::ifstream;
 using std::istringstream;
+using std::sort;
 using std::string;
 using std::vector;
 using std::abs;
 
-enum class State {kEmpty, kObstacle, kClosed};
+enum class State {kEmpty, kObstacle, kClosed, kPath};
+
+// directional deltas
+const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
 
 vector<State> ParseLine(string line) {
@@ -42,19 +47,40 @@ vector<vector<State>> ReadBoardFile(string path) {
   return board;
 }
 
-// TODO: Write function to compare the f-value of two nodes here
-bool Compare(vector<int> node1, vector<int> node2){
-	int f_val1 = node1[2] + node1[3];
-  	int f_val2 = node2[2] + node2[3];
-  
-  if (f_val1 > f_val2) {
-    return true;
-} else {
-    return false;
-}}
+
+/**
+ * Compare the F values of two cells.
+ */
+bool Compare(const vector<int> a, const vector<int> b) {
+  int f1 = a[2] + a[3]; // f1 = g1 + h1
+  int f2 = b[2] + b[3]; // f2 = g2 + h2
+  return f1 > f2; 
+}
+
+
+/**
+ * Sort the two-dimensional vector of ints in descending order.
+ */
+void CellSort(vector<vector<int>> *v) {
+  sort(v->begin(), v->end(), Compare);
+}
+
+
 // Calculate the manhattan distance
 int Heuristic(int x1, int y1, int x2, int y2) {
   return abs(x2 - x1) + abs(y2 - y1);
+}
+
+
+/** 
+ * Check that a cell is valid: on the grid, not an obstacle, and clear. 
+ */
+bool CheckValidCell(int x, int y, vector<vector<State>> &grid) {
+  bool on_grid_x = (x >= 0 && x < grid.size());
+  bool on_grid_y = (y >= 0 && y < grid[0].size());
+  if (on_grid_x && on_grid_y)
+    return grid[x][y] == State::kEmpty;
+  return false;
 }
 
 
@@ -67,6 +93,30 @@ void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openlist, vector
   grid[x][y] = State::kClosed;
 }
 
+
+/** 
+ * Expand current nodes's neighbors and add them to the open list.
+ */
+void ExpandNeighbors(const vector<int> &current, int goal[2], vector<vector<int>> &openlist, vector<vector<State>> &grid){
+	// current nodes data
+  	int x = current[0];
+  	int y = current[1];
+  	int g = current[2];
+  
+  	// loop through current node's neigbours
+  	for (int i = 0; i < 4; i++){
+     	int x2 = x + delta[i][0];
+      	int y2 = y + delta[i][1];
+      
+      // Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
+      if (CheckValidCell(x2, y2, grid)) {
+        // Increment g value and add neighbor to open list.
+        int g2 = g + 1;
+        int h2 = Heuristic(x2, y2, goal[0], goal[1]);
+        AddToOpen(x2, y2, g2, h2, openlist, grid);
+      }
+	}
+}
 
 /** 
  * Implementation of A* search algorithm
@@ -82,13 +132,34 @@ vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2
   int h = Heuristic(x, y, goal[0],goal[1]);
   AddToOpen(x, y, g, h, open, grid);
 
+  while (open.size() > 0) {
+    // Get the next node
+    CellSort(&open);
+    auto current = open.back();
+    open.pop_back();
+    x = current[0];
+    y = current[1];
+    grid[x][y] = State::kPath;
+
+    // Check if we're done.
+    if (x == goal[0] && y == goal[1]) {
+      return grid;
+    }
+    
+    // If we're not done, expand search to current node's neighbors.
+    ExpandNeighbors(current, goal, open, grid);
+  }
+  
+  // We've run out of new nodes to explore and haven't found a path.
   cout << "No path found!" << "\n";
   return std::vector<vector<State>>{};
 }
 
+
 string CellString(State cell) {
   switch(cell) {
     case State::kObstacle: return "⛰️   ";
+    case State::kPath: return "🚗   ";
     default: return "0   "; 
   }
 }
@@ -115,4 +186,7 @@ int main() {
   TestHeuristic();
   TestAddToOpen();
   TestCompare();
+  TestSearch();
+  TestCheckValidCell();
+  TestExpandNeighbors();
 }
